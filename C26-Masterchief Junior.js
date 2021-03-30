5b42adbcd5ab96f7e740f2551794306bb591d8b7
Ayushi1313 <78208221+Ayushi1313@users.noreply.github.com>
2021-03-30 17:22:44 +0530

    Add files via upload

---
+++ C26-Masterchief Junior.js
@@ -0,0 +1,19 @@
+Recipe of an omelette:-
+Ingredients- 
+1. 2 eggs
+2. salt
+3. pepper
+4. 1 onion
+5. 1 capcicum
+6. 1 tomato
+
+Method-
+1. crack two eggs in a bowl.
+2. chop the onion, tomato and capcicum. Add it to the bowl.
+3. add salt and pepper to bowl as per taste.
+4. mix the mixture well.
+5. butter the pan.
+6. gently pour the mixture in the pan.
+7. wait for 1 minute and carefully flip the omelette.
+8. after 30 more seconds, serve the warm omelette.
+9. enjoy with coffee or a drink.
